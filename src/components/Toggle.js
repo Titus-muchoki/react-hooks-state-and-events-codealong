4e146ec802334ade9e60aaf872861ecb8f245975
kajela <titoyut56@gmail.com>
2023-06-08 05:50:38 -0400

    add the initial files

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -6,9 +6,13 @@ function Toggle() {
   function handleClick(){
     setIsOn((isOn) => !isOn); 
   }
+  const color = isOn ? "red" : "white";
 
 
-  return <button onClick={handleClick}>{isOn ? "ON" : "OFF"}</button>;
+  return(
+<button style={{background: color}} onClick={handleClick}>
+  {isOn ? "ON" : "OFF"}</button>
+  );
 }
 
 export default Toggle;
